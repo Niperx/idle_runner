5837ec6639fe22884bfd7904893f04c3d92e77dd
Niperx <79987802+Niperx@users.noreply.github.com>
2021-07-29 10:21:53 +1100

    upd

--- aue_tg.py
+++ aue_tg.py
@@ -22,18 +22,29 @@ upload = VkUpload(vk_session)
 vk_groups = [-27456813, -156666557, -35927256]
 
 def get_quot():
+	err = True
 	grp = vk_groups[random.randint(0, 2)]
 	print(grp)
 	quot = session_api.wall.get(owner_id=grp, count=50, filter='owner', extended=1)
 	quot_list = []
-	while len(quot_list) < 25:
-		for x in quot['items']:
-			quot_list.append(x['text'])
-		result = quot_list[random.randint(0, len(quot_list))]
+	for x in quot['items']:
+		quot_list.append(x['text'])
+	
+	while err == True:	
+		try:
+			result = quot_list[random.randint(1, len(quot_list))]
+			if len(result) > 25 and 'http' not in result and 'подпис' not in result and 'vk.com' not in result and 'пригла' not in result:
+				# print('\nПОДХОДИТ\n')
+				err = False
+		except IndexError:
+			continue
+	# print(f'Фраза: {result}')
+	print(f'Список: {quot_list}')
 	return result
 
 
 def get_answer():
+
 	info = get_quot()
 	info = info.replace(', ',',\n')
 	info = info.replace(' и ','\nи ')
@@ -41,19 +52,19 @@ def get_answer():
 	info = info.replace('... ','...\n')
 	info = info.replace('" ','"\n')
 	info = info.replace("' ","'\n")
+	info = info.replace(' чем','\nчем')
+	info = info.replace('то что','то\nчто')
 
-	TINT_COLOR = (0, 0, 0)  # Black
-	TRANSPARENCY = .25  # Degree of transparency, 0-100%
-	OPACITY = int(255 * TRANSPARENCY)
-
+	maxsize = (1028, 1028)
 	img = Image.open('images/wolfs/wolf'+str(random.randint(1, 5))+'.jpg')
+	img.thumbnail(maxsize, Image.ANTIALIAS)
 	font = ImageFont.truetype('fonts/Bellota-Regular.ttf', size=42)
 	draw_text = ImageDraw.Draw(img, "RGBA")
 	w, h = draw_text.textsize(info, font)
 	x, y = (img.width / 2 - (w/2), img.height / 2 - (h/2))
 	
-	draw_text.rectangle((x - 10, y - 10, x + w + 20, y + h + 20), fill=(0, 0, 0, 90))
-	draw_text.rectangle((x - 10, y - 10, x + w + 20, y + h + 20), outline=(0, 0, 0, 127), width=2)
+	draw_text.rectangle((x - 10, y - 10, x + w + 30, y + h + 20), fill=(0, 0, 0, 150))
+	draw_text.rectangle((x - 10, y - 10, x + w + 30, y + h + 20), outline=(0, 0, 0, 127), width=2)
 	draw_text.text(
 		(x, y),
 		info.title(),
@@ -68,27 +79,20 @@ dp = Dispatcher(bot)
 
 
 
+# @dp.message_handler(text=['ауф'])
+# async def process_start_command(message: types.Message):
+#     await message.reply("Привет!\nАуф ёпта!")
 
-
-
+# @dp.message_handler(commands=['photo'])
 
 @dp.message_handler(text=['ауф'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nАуф ёпта!")
-
-
-@dp.message_handler(commands=['photo'])
+@dp.message_handler(commands=['auf'])
 async def process_photo_command(message: types.Message):
     caption = 'Какие глазки! :eyes:'
     get_answer()
     imageFile = r"images/wolfs/wolf.jpg"
     img = open(imageFile, 'rb')
-    await bot.send_photo(message.from_user.id, img, caption=emojize(caption), reply_to_message_id=message.message_id)
-
-
-
-
-
+    await bot.send_photo(message.from_user.id, img, reply_to_message_id=message.message_id)
 
 
 if __name__ == '__main__':
